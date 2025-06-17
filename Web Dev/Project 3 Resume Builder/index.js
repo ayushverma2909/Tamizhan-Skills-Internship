528114463e7bd2c569dfa1894ae47230763d7615
Ayush Verma <ayushv2909@gmail.com>
2025-06-18 01:02:13 +0530

    fixing resume builder

--- Web Dev/Project 3 Resume Builder/index.js
+++ Web Dev/Project 3 Resume Builder/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
 import { fileURLToPath } from "url";
-import multer from "multer";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,55 +9,32 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' })); 
 
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "/tmp");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
-    cb(null, uniqueSuffix + "-" + file.originalname);
-  }
-});
-
-const upload = multer({ storage: storage });
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
+
 app.get("/form2", (req, res) => {
-  const template = req.query.template || "1"; 
+  const template = req.query.template || "1";
   res.render("form2", { template, data: { template } });
-
-  // console.log(template)
-
 });
 
-
-app.post("/generate",upload.single("photo"), (req, res) => {
+app.post("/generate", (req, res) => {
   const data = req.body;
 
-  if (req.file) {
-    data.photo = "/tmp/" + req.file.filename;
-
-  }
+  data.photo = data.photoBase64 || "";
 
   if (data.template === "1") {
     res.render("resume1", { data, downloadMode: true });
   } else {
     res.render("resume2", { data, downloadMode: true });
   }
-  // console.log(data.template)
 });
 
 app.use((err, req, res, next) => {
@@ -66,7 +42,6 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
-
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
